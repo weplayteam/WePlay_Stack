c6d5f50c061b14aeec341ae0321f53d0b1ac61b7
AkashGutha <akash.gutha@outlook.com>
2019-06-12 01:54:34 +0530

    FIX: Server rendering issues

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const stripe = require("stripe")("sk_test_LCwlgZu3SBDKwfIHkZo9kw2x00SjMcYvQ2");
+const ReactDOMServer = require('react-dom/server');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(port, () => {
-    console.log(`Server started on port: ${port}`)
-})
+app.get('/*', (req, res) => {
+    const context = {};
+    const react_app = ReactDOMServer.renderToString(
+        <StaticRouter location={req.url} context={context}>
+            <App />
+        </StaticRouter>
+    );
 
-app.get("/", (req, res)=>{
-    res.render("index");
-})
+    const indexFile = path.resolve('./weplay_client/public/index.html');
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Something went wrong:', err);
+            return res.status(500).send('Oops, better luck next time!');
+        }
+
+        return res.send(
+            data.replace('<div id="root"></div>', `<div id="root">${react_app}</div>`)
+        );
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`😎 Server is listening on port ${PORT}`);
+});
--- weplay_client/src/index.js
+++ weplay_client/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.hydrate(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
